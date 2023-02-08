666ce8796275a6f14f8a539fd07a55e2e6477461
vinayakgarole <vinayakgarole10@gamil.com>
2023-02-08 15:28:29 +0530

    Modified: Logic to push into stack

---
+++ src/com/bridgelabz/StackMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class StackMain {
+
+    public static void main(String[] args) {
+
+        StackService list = new StackService();
+        list.push(70);
+        list.push(30);
+        list.push(56);
+        list.printList();
+    }
+}
--- src/com/bridgelabz/StackService.java
+++ src/com/bridgelabz/StackService.java
@@ -2,8 +2,31 @@ package com.bridgelabz;
 
 public class StackService {
 
-    public static void main(String[] args) {
+    Node head;
+    int count = 1;
 
-        System.out.println("Welcome to Stack and Queue Program");
+    public void push(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void printList() {
+
+        if (head == null) {
+            System.out.println("Stack is Empty");
+            return;
+        }
+        Node currNode = head;
+
+        while (currNode != null) {
+            System.out.println(currNode.data + " ");
+            currNode = currNode.next;
+        }
     }
 }
